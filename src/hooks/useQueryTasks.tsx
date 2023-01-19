2a0ba4899f53962448442768ba91420d04b1feae
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-20 00:55:37 +0900

    refactor: リダイレクトを抑える

--- src/hooks/useQueryTasks.tsx
+++ src/hooks/useQueryTasks.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { Task } from '../types/types'
 
 const getTasks = async () => {
-  const { data } = await axios.get<Task[]>('http://127.0.0.1:8000/api/tasks')
+  const { data } = await axios.get<Task[]>('http://127.0.0.1:8000/api/tasks/')
   return data
 }
 
