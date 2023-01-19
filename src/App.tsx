08d1cbeaf16334667561a270fdcb5428f8f623c1
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-20 00:51:46 +0900

    feat: react-queryでのデータフェッチのテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import { FC } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ClassicalFetchA } from './components/ClassicalFetchA'
 import { ClassicalFetchB } from './components/ClassicalFetchB'
 import { StateProvider } from './context/StateProvider'
 import { Layout } from './components/Layout'
+import { ReactQueryA } from './components/ReactQueryA'
+import { ReactQueryB } from './components/ReactQueryB'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <StateProvider>
-        <Layout>
-          <Routes>
-            <Route path="/fetch-a" element={<ClassicalFetchA />} />
-            <Route path="/fetch-b" element={<ClassicalFetchB />} />
-          </Routes>
-        </Layout>
-      </StateProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <StateProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ReactQueryA />} />
+              <Route path="/query-b" element={<ReactQueryB />} />
+              <Route path="/fetch-a" element={<ClassicalFetchA />} />
+              <Route path="/fetch-b" element={<ClassicalFetchB />} />
+            </Routes>
+          </Layout>
+        </StateProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   )
 }
 
---
+++ src/components/ReactQueryB.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { ChevronDoubleLeftIcon } from '@heroicons/react/solid'
+import { Task } from '../types/types'
+
+export const ReactQueryB: FC = () => {
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
+  const data = queryClient.getQueryData<Task[]>(['tasks'])
+
+  console.log('rendered ReactQueB')
+
+  return (
+    <>
+      <p className="font-bold my-3">ReactQueryA</p>
+      {data?.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+      <ChevronDoubleLeftIcon
+        onClick={() => navigate('/')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">react query B</p>
+    </>
+  )
+}
